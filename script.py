2c99b8e2fedf627f0af9bc9d63b5b7bfa1521357
MiguelGPereira <miguelgopereira@gmail.com>
2015-10-27 22:13:05 +0100

    Convert image to array

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import imghdr
 import struct
+import math
 
 from sklearn import datasets, svm, metrics
 
@@ -46,13 +47,41 @@ def load_dataset(img_dir):
                 images.append(img_dir + superclass + "/" + subclass + "/" + image)
                 labels.append([superclass + "/" + subclass])
 
-    image_x_y_mean = map(lambda x: x/n_images, image_x_y_mean)
-    print(list(image_x_y_mean))
-    #for image in images:
-        #img = img_to_matrix(image)
-        #img = flatten_image(img)
-        #data.append(img)
-        #data = np.array(data)
+    image_x_y_mean = list(map(lambda x: math.floor(x/n_images), image_x_y_mean))
+    print(image_x_y_mean)
+
+    #setup a standard image size; this will distort some images but will get everything into the same shape
+    STANDARD_SIZE = image_x_y_mean
+    def img_to_matrix(filename, verbose=False):
+        """
+        takes a filename and turns it into a numpy array of RGB pixels
+        """
+        img = Image.open(filename)
+        #if verbose==True:
+            #print ("changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE)))
+        img = img.resize(STANDARD_SIZE)
+        img = list(img.getdata())
+        #img = map(list, img)
+        img = np.array(img)
+        return img
+
+    def flatten_image(img):
+        """
+        takes in an (m, n) numpy array and flattens it
+        into an array of shape (1, m * n)
+        """
+        s = img.shape[0] * img.shape[1]
+        img_wide = img.reshape(1, s)
+        return img_wide[0]
+
+    data = []
+    for image in images:
+        img = img_to_matrix(image)
+        img = flatten_image(img)
+        data.append(img)
+
+    data = np.array(data)
+    print(data[0])
 
 
 '''
