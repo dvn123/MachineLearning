bcc157a422d4c3907225092ef62f9b07daebbfba
MiguelGPereira <miguelgopereira@gmail.com>
2015-11-26 20:24:59 +0100

    Optimization of HOG parameters

--- image_features.py
+++ image_features.py
@@ -25,13 +25,11 @@ def flatten_image(img):
 
 
 def randomized_pca(train_data_images, train_data_split_images, test_data_images, IMG_SIZE):
-    train_data = []
-    train_data_split_crossfold = []
-    test_data = []
-
-    train_data_split_crossfold_features = []
     train_data_features = []
     test_data_features = []
+    train_data = []
+    test_data = []
+    train_data_split_crossfold = []
 
     for image in train_data_images:
         img = img_to_matrix(image, IMG_SIZE)
@@ -49,16 +47,14 @@ def randomized_pca(train_data_images, train_data_split_images, test_data_images,
         test_data.append(img)
 
     pca = RandomizedPCA(50)
-    return (pca.fit_transform(train_data), pca.transform(train_data_split_crossfold), pca.transform(test_data))
+    return (pca.fit_transform(train_data), pca.transform(test_data))
 
 def sift(train_data_images, train_data_split_images, test_data_images):
-    train_data = []
-    train_data_split_crossfold = []
-    test_data = []
-
-    train_data_split_crossfold_features = []
     train_data_features = []
     test_data_features = []
+    train_data = []
+    test_data = []
+    train_data_split_crossfold = []
     print(4)
     bow_train = cv2.BOWKMeansTrainer(8)
 
@@ -108,13 +104,11 @@ def sift(train_data_images, train_data_split_images, test_data_images):
         if a is None:
             continue
         train_data_features.append(a.flatten())
-
-    for image in train_data_split_crossfold:
-        a = bow_extract.compute(image, detect.detect(image))
-        if a is None:
-            continue
-        train_data_split_crossfold_features.append(a.flatten())
-
+    print(len(train_data_features))
+    #for image in train_data_split_crossfold:
+    #bowDes = bow_extract.compute(image, kps, descs)
+    #train_data_features.append(bow_extract.compute(image, detect.detect(image)))
+    #train_data_features.append(descs)
     for image in test_data:
         #(kps, descs) = detect.detectAndCompute(image, None)
         #bowDes = bow_extract.compute(image, kps, descs)
@@ -123,11 +117,22 @@ def sift(train_data_images, train_data_split_images, test_data_images):
             continue
         test_data_features.append(a.flatten())
 
-    return np.asarray(train_data_features), np.asarray(train_data_split_crossfold_features), np.asarray(test_data_features)
+    return np.asarray(train_data_features), np.asarray(test_data_features)
 
 def hog_features(train_data_images, train_data_split_images, test_data_images, IMG_SIZE):
+    train_data_features = []
+    test_data_features = []
     train_data = []
+    test_data = []
     train_data_split_crossfold = []
+
+    IMG_SIZE = (20,20)
+    orientations = 8
+    pixels_per_cell = (5, 5)
+    cells_per_block = (2, 2)
+    visualize = False
+    normalise = False
+
     test_data = []
 
     train_data_split_crossfold_features = []
@@ -149,15 +154,15 @@ def hog_features(train_data_images, train_data_split_images, test_data_images, I
         test_data.append(img)
 
     for image in train_data:
-        fd = hog(image)
+        fd = hog(image,orientations,pixels_per_cell,cells_per_block,visualize,normalise)
         train_data_features.append(fd)
 
     for image in train_data_split_crossfold:
-        fd = hog(image)
+        fd = hog(image,orientations,pixels_per_cell,cells_per_block,visualize,normalise)
         train_data_split_crossfold_features.append(fd)
 
     for image in test_data:
-        fd = hog(image)
+        fd = hog(image,orientations,pixels_per_cell,cells_per_block,visualize,normalise)
         test_data_features.append(fd)
 
     return np.array(train_data_features), np.array(train_data_split_crossfold_features), np.array(test_data_features)
--- machine_learning_models.py
+++ machine_learning_models.py
@@ -95,25 +95,33 @@ def cross_test(train_data_features, train_data_split_crossfold_features, test_da
 
 def linear_svm(train_data_features, train_data_split_crossfold_features, test_data_features, labels, labels_cross_validation_classwise, using_cross_validation2, kf):
     if using_cross_validation2:
-        C_base = 0.025
-        C_step = 0.005
+        C_base = 4.5
+        C_step = 0.5#0.005
         C = C_base
         _results = []
         for train, test in kf:
             svc = SVC(kernel="linear", C=C, probability=True)
             model = svc.fit(train_data_features[train], labels[train])
             predicted_classes = model.predict(train_data_features[test])
+            predicted_classes_train = model.predict(train_data_features[train])
             class_probabilities = model.predict_proba(train_data_features[test])
-            print("n points:", len(predicted_classes), ", wrong: ", (labels[test] != predicted_classes).sum(), " percentage: ",(labels[test] != predicted_classes).sum()*100/len(predicted_classes),"%")
+            print("n points:", len(predicted_classes), " percentage: ",(labels[test] != predicted_classes).sum()*100/len(predicted_classes),"% percentage_train: ", (labels[train] != predicted_classes_train).sum()*100/len(predicted_classes_train),"%")
             _results.append((labels[test] != predicted_classes).sum())
             C += C_step
         C = C_base + C_step * _results.index(min(_results))
         print("C: ", C)
+        if(len(train_data_split_crossfold_features) > 0):
+            svc = SVC(kernel="linear", C=C, probability=True)
+            model = svc.fit(train_data_features, labels)
+            predicted_classes = model.predict(train_data_split_crossfold_features)
+            class_probabilities = model.predict_proba(train_data_split_crossfold_features)
+            print("N points:", len(predicted_classes), " percentage: ",(labels_cross_validation_classwise != predicted_classes).sum()*100/len(predicted_classes),"%")
+            print("Log_loss: ", log_loss(labels_cross_validation_classwise, class_probabilities))
         svc = SVC(kernel="linear", C=C, probability=True)
         model = svc.fit(train_data_features, labels)
         return model.predict_proba(test_data_features), model.predict(test_data_features), model
     else:
-        svc = SVC(kernel="linear", C=0.025, probability=True)
+        svc = SVC(kernel="linear", C=8, probability=True)
         model = svc.fit(train_data_features, labels)
         return model.predict_proba(test_data_features), model.predict(test_data_features), model
 
